7501386177df4f69703e30980a303e8db4dd6ec1
yash0713 <20je1110@cse.iitism.ac.in>
2024-01-18 00:17:36 +0530

    feature added
    + error logging
    + connection termination by 'bye'
    + connection termination in event of NO DATA
    + MAC address provided as parameter

--- client.py
+++ client.py
@@ -1,17 +1,42 @@
 import socket
+import sys
+import re
+
+def is_valid_mac(mac_address):
+    # Regular expression to validate MAC address format
+    mac_pattern = re.compile(r'^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$')
+    return bool(mac_pattern.match(mac_address))
+
+if len(sys.argv) != 2 or not is_valid_mac(sys.argv[1]):
+    print("Usage: python script_name.py <server_mac_address>")
+    print("Please check the MAC address format.")
+    sys.exit(1)
+
+server_mac_address = sys.argv[1]
 
 client = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-client.connect(("c8:e2:65:cd:d7:fe",4))
 
 try:
-	while True:
-		message = input("Client: ")
-		client.send(message.encode("utf-8"))
-		data = client.recv(1024)
-		if not data:
-			break
-		print(f"Server: {data.decode('utf-8')}")
-except OSError as e:
-	pass
+    client.connect((server_mac_address, 4))
+    print(f"Connected to server at {server_mac_address}")
 
-client.close()
+    while True:
+        message = input("Client: ")
+        client.send(message.encode("utf-8"))
+
+        if message.lower() == "bye":
+            print("Closing the connection. Goodbye!")
+            break
+
+        data = client.recv(1024)
+        if not data:
+            print("Server closed the connection. Goodbye!")
+            break
+
+        print(f"Server: {data.decode('utf-8')}")
+
+except OSError as e:
+    print(f"Error: {e}")
+
+finally:
+    client.close()
--- server.py
+++ server.py
@@ -1,21 +1,43 @@
 import socket
 
 server = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-server.bind(("C8:E2:65:CD:D7:FE", 4))
-server.listen(1)
-
-client, addr = server.accept()
 
 try:
-	while True:
-		data = client.recv(1024)
-		if not data:
-			break
-		print(f"Client: {data.decode('utf-8')}")
-		message = input("Server:")
-		client.send(message.encode("utf-8"))
-except OSError as e:
-	pass
+    server.bind(("C8:E2:65:CD:D7:FE", 4))
+    server.listen(1)
+    print("Waiting for incoming connections...")
 
-client.close()
-server.close()
+    client, addr = server.accept()
+    print(f"Connected to {addr}")
+
+    try:
+        while True:
+            data = client.recv(1024)
+            if not data:
+                break
+
+            message = data.decode('utf-8')
+            print(f"Client: {message}")
+
+            if message.lower() == "bye":
+                print("Client requested to close the connection. Closing...")
+                break
+
+            response = input("Server:")
+            client.send(response.encode("utf-8"))
+
+    except ConnectionResetError:
+        print("Connection reset by the client.")
+    except ConnectionAbortedError:
+        print("Connection aborted.")
+    except OSError as e:
+        print(f"Error: {e}")
+    finally:
+        client.close()
+
+except OSError as e:
+    print(f"Socket error: {e}")
+
+finally:
+    server.close()
+    print("Server closed.")
