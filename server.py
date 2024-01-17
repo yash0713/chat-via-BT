15a0dc7d7a3d7b7329ed445154cd58bc6bc8e703
yash0713 <20je1110@cse.iitism.ac.in>
2024-01-17 11:55:33 +0530

    files addition

---
+++ server.py
@@ -0,0 +1,21 @@
+import socket
+
+server = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+server.bind(("C8:E2:65:CD:D7:FE", 4))
+server.listen(1)
+
+client, addr = server.accept()
+
+try:
+	while True:
+		data = client.recv(1024)
+		if not data:
+			break
+		print(f"Message: {data.decode('utf-8')}")
+		message = input("Enter message:")
+		client.send(message.encode("utf-8"))
+except OSError as e:
+	pass
+
+client.close()
+server.close()
